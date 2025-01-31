5dfbda6ce4e132ec8059ec21ae87c9626693591c
Kranthi Atla <Kranthiatla95@gmail.com>
2025-01-31 10:05:40 +0530

    intial commit

--- store/models.py
+++ store/models.py
@@ -10,43 +10,49 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title=models.CharField(max_length=255)
+    slug=models.SlugField()
     description=models.TextField()
-    price=models.DecimalField(max_digits=6,decimal_places=2)
+    unit_price=models.DecimalField(max_digits=6,decimal_places=2)
     inventory=models.IntegerField()
     last_update=models.DateTimeField(auto_now=True)
     collection=models.ForeignKey(Collection,on_delete=models.PROTECT)
     promotions=models.ManyToManyField(Promotion)
 
 class Customer(models.Model):
-    MEMBERSHIP_BRONZE='B',
-    MEMBERSHIP_SILVER='S',
-    MEMBERSHIP_GOLD='G',
-
-    MEMBERSHIP_CHOICES=[
-        (MEMBERSHIP_BRONZE,'Bronze'),
-        (MEMBERSHIP_SILVER,'Silver'),
-        (MEMBERSHIP_GOLD,'Gold'),
+    MEMBERSHIP_BRONZE = 'B'
+    MEMBERSHIP_SILVER = 'S'
+    MEMBERSHIP_GOLD = 'G'
+    MEMBERSHIP_CHOICES = [
+        (MEMBERSHIP_BRONZE, 'Bronze'),
+        (MEMBERSHIP_SILVER, 'Silver'),
+        (MEMBERSHIP_GOLD, 'Gold'),
     ]
     first_name=models.CharField(max_length=255)
     last_name=models.CharField(max_length=255)
     email=models.EmailField(unique=True)
     phone=models.CharField(max_length=255)
     birth_date=models.DateField(null=True)
-    membership=models.CharField(max_length=255,choices='MEMBERSHIP_CHOICES',default=MEMBERSHIP_BRONZE)  
+    membership=models.CharField(max_length=255,choices=MEMBERSHIP_CHOICES,default=MEMBERSHIP_BRONZE)
+    
+    class Meta:
+        db_table = 'store_customers'
+        indexes = [
+            models.Index(fields=['last_name', 'first_name']),
+            models.Index(fields=['birth_date']),
+        ]  
 
 class Order(models.Model):
-    PAYMENT_STATUS_PENDING='P',
-    PAYMENT_STATUS_COMPLETE='C',
-    PAYMENT_STATUS_FAILED='F',
-
-    PAYMENT_CHOICES=[
-        (PAYMENT_STATUS_PENDING,'Pending'),
-        (PAYMENT_STATUS_COMPLETE,'Complete'),
-        (PAYMENT_STATUS_FAILED,'Failed'),
+   PAYMENT_PENDING = 'P'
+   PAYMENT_COMPLETE = 'C'
+   PAYMENT_FAILED = 'F' 
+   PAYMENT_CHOICES = [
+        (PAYMENT_PENDING, 'Pending'),
+        (PAYMENT_COMPLETE, 'Complete'),
+        (PAYMENT_FAILED, 'Failed'),
     ]
-    placed_at=models.DateTimeField(auto_now_add=True)
-    payment_status=models.CharField(max_length=255,choices='PAYMENT_CHOICES',default=PAYMENT_STATUS_PENDING)
-    customer=models.ForeignKey(Customer,on_delete=models.PROTECT)
+   placed_at=models.DateTimeField(auto_now_add=True)
+   payment_status=models.CharField(max_length=255,choices=PAYMENT_CHOICES,default=PAYMENT_PENDING)
+   customer=models.ForeignKey(Customer,on_delete=models.PROTECT)
     
 class OrderItem(models.Model):
     order=models.ForeignKey(Order,on_delete=models.PROTECT)
